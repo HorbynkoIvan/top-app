ae673ed9908655e228d006d4bfdea86039fbef4b
HorbynkoIvan <horbynko.i@gmail.com>
2021-11-01 09:55:29 +0200

    added HTMLprops

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h3">sometext</Htag>
-      <Button appearance="primary">Button</Button>
+      <Button appearance="primary" className="test">
+        Button
+      </Button>
       <Button appearance="ghost">Button</Button>
     </div>
   );
